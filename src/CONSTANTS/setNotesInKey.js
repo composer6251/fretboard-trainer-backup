e403e98a22b03ae298092087d4b69d4f42c414ef
composer6251 <composercoder57>
2022-03-06 12:56:27 -0500

    Initial Commit

--- src/CONSTANTS/setNotesInKey.js
+++
@@ -1,7 +0,0 @@
-/*********
- * This Class will take in 
- * 3 Params:
- * @keyNote, @numOfOctaves, @SCALE_ARRAY
- * It will then generate the notesInKey array
- *
- */
--- src/globals/global-const/constants.jsx
+++
@@ -1,5 +0,0 @@
-
-
-//Login and Authentication
-export const GUEST_USERNAME = "Guest";
-export const AUTHENTICATED = "Authenticated";
